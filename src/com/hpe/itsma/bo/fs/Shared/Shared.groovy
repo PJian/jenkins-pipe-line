a64ac1f60b01d5854141098fe65dd50cfa8f8d5f
peijian <peijian214@126.com>
2017-06-27 21:12:15 +0800

    improve

--- src/com/hpe/itsma/bo/fs/Shared/Shared.groovy
+++ src/com/hpe/itsma/bo/fs/Shared/Shared.groovy
@@ -1,3 +1,9 @@
-def static test(String msg){
-    println "${msg}"
+package com.hpe.itsma.bo.fs.Shared
+class Utilities {
+    def static testStatic(String msg){
+        println "${msg}"
+    }
+    def testInstance(msg){
+        println "${msg}"
+    }
 }
